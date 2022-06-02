5c9bf8c1cb6c32b39ddb68dc38061465bdda348c
Adriano Nazario <adriano.bnazario@gmail.com>
2022-06-01 21:48:34 -0300

    html e css

---
+++ script.js
@@ -0,0 +1,23 @@
+// Retorne o url da página atual utilizando o objeto window
+
+
+console.log(window.location.href)
+
+
+
+
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const titulo = document.querySelector(".titulo22");
+
+console.log(titulo)
+
+// Retorne a linguagem do navegador
+
+console.log(navigator.language)
+
+
+// Retorne a largura da janela 
+
+console.log(titulo.offsetHeight)
